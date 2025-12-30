bf3f041e6de1ee9cd00f7e0cbb5a06d79ddd5915
Himanshu wadhwa <himanshuhw999@gmail.com>
2025-12-30 22:49:52 +0530

    update: env fetching and table creation for users

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,16 @@
-from typing import List
-from typing import Optional
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
-
-class Base(DeclarativeBase):
-    pass
+import uuid
+from sqlalchemy import Column, String, DateTime
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from app.config.db import Base
 
 class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
-    addresses: Mapped[List["Address"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
-    )
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    __tablename__ = "users"
+
+    id:int = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email:str = Column(String, unique=True, nullable=False)
+    name:str = Column(String)
+    google_id:str = Column(String, unique=True, nullable=False)
+    avatar_url:str = Column(String)
+    created_at:datetime = Column(DateTime(timezone=True), server_default=func.now())
