d16566c485fefa844aa83a83fa92d5b818484eef
Himanshu wadhwa <himanshuhw999@gmail.com>
2026-01-11 18:34:25 +0530

    adding token generating methods

--- app/core/security.py
+++ app/core/security.py
@@ -1,20 +1,36 @@
 from fastapi import HTTPException
 from datetime import datetime, timedelta
+import hashlib
 from jose import jwt,JWTError
 import requests
-from app.config.env import JWT_EXPIRE_DAYS,JWT_SECRET_KEY, JWT_ALGORITHM
+from app.config.env import JWT_EXPIRE_DAYS,JWT_SECRET_KEY, JWT_ALGORITHM,JWT_EXPIRE_MINUTES
 
 GOOGLE_CERTS_URL = "https://www.googleapis.com/oauth2/v3/certs"
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(days=JWT_EXPIRE_DAYS)
+    to_encode["type"] = "access"
+    expire = datetime.utcnow() + timedelta(days=JWT_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(
         to_encode,
         JWT_SECRET_KEY,
         algorithm=JWT_ALGORITHM
     )
+
+def create_refresh_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=JWT_EXPIRE_DAYS)
+    to_encode["type"] = "refresh"
+    to_encode.update({"exp":expire})
+    return jwt.encode(
+        to_encode,
+        JWT_SECRET_KEY,
+        algorithm=JWT_ALGORITHM
+    )
+
+def hash_token(token:str)->str:
+    return hashlib.sha256(token.encode()).hexdigest()
 
 def verify_google_token(token:str,client_id:str):
     certs = requests.get(GOOGLE_CERTS_URL).json()
